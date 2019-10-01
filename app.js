3569e3a36426c504986df2cceecc9585093c17d5
Hannah Chamorro <hannah.chamorro@gmail.com>
2019-10-01 14:34:30 -0500

    copy button

--- app.js
+++ app.js
@@ -53,28 +53,62 @@ let letter = [
   "Y",
   "Z"
 ];
-let specialCharacter = ["!", "#", "$", "&", "8"];
+let specialCharacter = ["!", "#", "$", "&", "*"];
 let number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 let pwLen = prompt("How long do you want your password?");
 let pwNum = confirm("Do you want a numbers?");
+let pwChar = confirm("Do you want a special character?");
 
+function chanceSymbol(arr) {
+  return Math.floor(Math.random() * arr.length);
+}
 function showNewPassword() {
-  if (pwNum === true) {
+  if (pwChar === true && pwNum === true) {
     for (let i = 0; i < pwLen; i++) {
-      let addNum = Math.floor(Math.random() * number.length);
-      let letterLength = Math.floor(Math.random() * letter.length);
-      pw = pw + number[addNum] + letter[letterLength];
+      let addNum = chanceSymbol(number);
+      let addLetter = chanceSymbol(letter);
+      let addChar = chanceSymbol(specialCharacter);
+      pw = pw + number[addNum] + letter[addLetter] + specialCharacter[addChar];
+    }
+  } else if (pwNum === true) {
+    for (let i = 0; i < pwLen; i++) {
+      let addNum = chanceSymbol(number);
+      let addLetter = chanceSymbol(letter);
+      pw = pw + number[addNum] + letter[addLetter];
     }
   } else {
     for (let i = 0; i < pwLen; i++) {
-      let letterLength = Math.floor(Math.random() * letter.length);
-      pw = pw + letter[letterLength];
+      let addLetter = chanceSymbol(letter);
+      pw = pw + letter[addLetter];
     }
   }
 
   pwBox.innerHTML = pw;
 }
-genBtn.addEventListener("click", showNewPassword);
+
+function copyClipboard() {
+  var elm = document.getElementById("pwBox");
+  // for Internet Explorer
+
+  if (document.body.createTextRange) {
+    var range = document.body.createTextRange();
+    range.moveToElementText(elm);
+    range.select();
+    document.execCommand("Copy");
+    alert("Copied password to clipboard");
+  } else if (window.getSelection) {
+    // other browsers
+
+    var selection = window.getSelection();
+    var range = document.createRange();
+    range.selectNodeContents(elm);
+    selection.removeAllRanges();
+    selection.addRange(range);
+    document.execCommand("Copy");
+    alert("Copied password to clipboard");
+  }
+}
 /* first event */
 // change this pw into a dynamic random pw
 //document.write(pw);

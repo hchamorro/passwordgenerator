14922369e4a6d32c58650184d5aa272f7dae67c0
Hannah Chamorro <hannah.chamorro@gmail.com>
2019-09-30 20:31:44 -0500

    numbers and letters

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var pw = "";
-var letter = [
+let pw = "";
+let letter = [
   "a",
   "b",
   "c",
@@ -25,14 +25,53 @@ var letter = [
   "w",
   "x",
   "y",
-  "z"
+  "z",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z"
 ];
-var pwLen = prompt("How long do you want your password?");
+let specialCharacter = ["!", "#", "$", "&", "8"];
+let number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let pwLen = prompt("How long do you want your password?");
+let pwNum = confirm("Do you want a numbers?");
+
 function showNewPassword() {
-  for (var i = 0; i < pwLen; i++) {
-    var number = Math.floor(Math.random() * letter.length);
-    pw = pw + letter[number];
+  if (pwNum === true) {
+    for (let i = 0; i < pwLen; i++) {
+      let addNum = Math.floor(Math.random() * number.length);
+      let letterLength = Math.floor(Math.random() * letter.length);
+      pw = pw + number[addNum] + letter[letterLength];
+    }
+  } else {
+    for (let i = 0; i < pwLen; i++) {
+      let letterLength = Math.floor(Math.random() * letter.length);
+      pw = pw + letter[letterLength];
+    }
   }
+
   pwBox.innerHTML = pw;
 }
 genBtn.addEventListener("click", showNewPassword);
